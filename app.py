fb70a2a452d0d337d6116672bebb2c9d511ce393
Prafullabhosale <prafullabhosale630@gmail.com>
2026-04-09 11:32:10 +0530

    Update app.py
    Added 20,50,200

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ st.title("📈 Personal Market Guide")
 
 def get_trend(symbol):
     data = yf.download(symbol, period="3mo", interval="1d") 
-    data["EMA20"] = data["Close"].ewm(span=20, adjust=False).mean()
+    data["EMA20"] = data["Close"].ewm(span=20, adjust=False).mean() 
+    data["EMA50"] = data["Close"].ewm(span=50, adjust=False).mean()
     data["EMA200"] = data["Close"].ewm(span=200, adjust=False).mean()
 
     # If no data
